12469ce1428a36b954de5d0cb11ef69a753f1fce
wesley <wesleyfaraujjo@gmail.com>
2023-12-07 22:41:36 -0300

    adding navigation to the bot

--- PlainTextMessageReceiver.cs
+++ PlainTextMessageReceiver.cs
@@ -7,33 +7,19 @@ using Take.Blip.Client;
 
 namespace Blip_sdk
 {
-    /// <summary>
-    /// Defines a class for handling messages. 
-    /// This type must be registered in the application.json file in the 'messageReceivers' section.
-    /// </summary>
     public class PlainTextMessageReceiver : IMessageReceiver
     {
         private readonly ISender _sender;
-        private readonly Settings _settings;
 
-        public PlainTextMessageReceiver(ISender sender, Settings settings)
+        public PlainTextMessageReceiver(ISender sender)
         {
             _sender = sender;
-            _settings = settings;
         }
 
         public async Task ReceiveAsync(Message message, CancellationToken cancellationToken)
         {
             Trace.TraceInformation($"From: {message.From} \tContent: {message.Content}");
-
-            // Pega a mensagem enviada para o bot
-            Console.WriteLine(message.Content.ToString());
-
-            await _sender.SendMessageAsync("Bem vindo! Ao comprador de ingressos online.", message.From, cancellationToken);
-
-            // Tempo para mostrar a proxima mensagem
-            Thread.Sleep(1000);
-            await _sender.SendMessageAsync("Como posso ajudar?", message.From, cancellationToken);
+            await _sender.SendMessageAsync("Pong!", message.From, cancellationToken);
         }
     }
 }
